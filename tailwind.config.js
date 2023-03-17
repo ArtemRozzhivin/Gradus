e27ade9dd7100f7db0d98200bd0cb560f0c3124a
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2023-03-17 23:37:37 +0200

    links for footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,14 +15,9 @@ module.exports = {
 				'container': "1100px"
 			}, 
 
-// 			.color1 {color: #000219;}
-// .color2 {color: #003458;}
-// .color3 {color: #0069a5;}
-// .color4 {color: #00aaff;}
-// .color5 {color: #00f6ff;}
 			colors: {
-				'app': '#00759d',
-				'second': '#00b0d1',
+				'app': '#0c262c',
+				'second': '#5f8e5b',
 				'background': '#002642'
 			},
 		},
