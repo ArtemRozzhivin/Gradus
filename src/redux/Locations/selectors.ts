02e94466c6bd4019fa9382fa9e54352833046d91
Artem Rozzhyvin <91954776+ArtemRozzhivin@users.noreply.github.com>
2024-08-18 18:11:26 +0300

    Delete src/redux/Locations directory

--- src/redux/Locations/selectors.ts
+++
@@ -1,3 +0,0 @@
-import { RootState } from '../store';
-
-export const selectLocations = (state: RootState) => state.locations;
--- src/redux/Locations/types.ts
+++
@@ -1,9 +0,0 @@
-export type Locations = {
-  lat: number;
-  lon: number;
-};
-
-export type LocationsSliceType = {
-  userLocation: Locations;
-  currentLocation: Locations;
-};
